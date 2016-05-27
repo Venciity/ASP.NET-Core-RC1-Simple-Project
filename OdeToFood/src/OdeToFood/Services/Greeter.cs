0567f59ffbc855fdaf48095a4c7ea78551777dc2
Ventsislav Yordanov <venciityjordanov@gmail.com>
2016-05-27 23:43:50 +0300

    Created a Greeting Service
    Notes:
    This raises the abstraction of the project.
    The "Configure" method in Startup.cs class for a parameter that
    implement interface so it doesn't know the ultimate type of greeter.
    That's configured in the application.

---
+++ OdeToFood/src/OdeToFood/Services/Greeter.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace OdeToFood.Services
+{
+    public class Greeter : IGreeter
+    {
+        private string greeting;
+
+        public Greeter(IConfiguration configuration)
+        {
+            this.greeting = configuration["greetingMessage"];
+        }
+
+        public string GetGreeting()
+        {
+            return greeting;
+        }
+    }
+}
