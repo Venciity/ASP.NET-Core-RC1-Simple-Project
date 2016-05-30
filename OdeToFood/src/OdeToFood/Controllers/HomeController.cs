2a6bf831d15c96e2d50723d55ebb4375d5652f9a
Ventsislav Yordanov <venciityjordanov@gmail.com>
2016-05-30 21:53:12 +0300

    Added ViewModel for the home page
    Separate entities from view models. Added a new model (view model) for
    the home page. Now we displaying all the restaurant and the current
    greeting for the application in the home page.

--- OdeToFood/src/OdeToFood/Controllers/HomeController.cs
+++ OdeToFood/src/OdeToFood/Controllers/HomeController.cs
@@ -1,20 +1,25 @@
 ﻿using Microsoft.AspNet.Mvc;
 using OdeToFood.Services;
+using OdeToFood.ViewModels;
 
 namespace OdeToFood.Controllers
 {
     public class HomeController : Controller
     {
-        public HomeController(IRestaurantData restaurantData)
+        public HomeController(IRestaurantData restaurantData, IGreeter gretter)
         {
             this.restaurantData = restaurantData;
+            this.greeter = gretter;
         }
 
         private IRestaurantData restaurantData;
+        private IGreeter greeter;
 
         public ViewResult Index()
         {
-            var model = this.restaurantData.GetAll();
+            var model = new HomePageViewModel();
+            model.Restaurants = this.restaurantData.GetAll();
+            model.CurrentGreeting = this.greeter.GetGreeting();
             return this.View(model);
         }
     }
