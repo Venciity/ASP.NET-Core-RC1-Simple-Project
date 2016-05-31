9ccd0c248b2be8c171637b5cc5e15da58a81f93d
Ventsislav Yordanov <venciityjordanov@gmail.com>
2016-06-01 00:58:08 +0300

    Implemented a DbContext
    Added an OdeToFoodDbContext that will give us access to a database.
    Added a SqlRestaurantData class which we will use in our controllers to
    talk to the database.
    
    Notes: To use the Entity Framework, we need to create a class that
    derives from the Entity Framework DBContext base class. Each DBContext
    class that you write will give you access to a single database.

---
+++ OdeToFood/src/OdeToFood/Services/SqlRestaurantData.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using OdeToFood.Entities;
+using System.Linq;
+
+namespace OdeToFood.Services
+{
+    public class SqlRestaurantData : IRestaurantData
+    {
+        public SqlRestaurantData(OdeToFoodDbContext context)
+        {
+            this.context = context;
+        }
+
+        private OdeToFoodDbContext context;
+
+        public void Add(Restaurant restaurant)
+        {
+            this.context.Add(restaurant);
+            this.context.SaveChanges();
+        }
+
+        public Restaurant Get(int id)
+        {
+            return this.context.Restaurants.FirstOrDefault(r => r.Id == id);
+        }
+
+        public IEnumerable<Restaurant> GetAll()
+        {
+            return this.context.Restaurants;
+        }
+    }
+}
