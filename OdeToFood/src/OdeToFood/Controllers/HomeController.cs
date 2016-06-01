5676009da60d5a6f4f5ce8df4d068b3607ea3167
Ventsislav Yordanov <venciityjordanov@gmail.com>
2016-06-01 20:33:52 +0300

    Added a view component
    Now we display the configurable greeting on every page of the
    application at the bottom of the page. There is no responsibility of the
    "Index" view to display this greeting. So we use the view component to
    display this configurable greeting.

--- OdeToFood/src/OdeToFood/Controllers/HomeController.cs
+++ OdeToFood/src/OdeToFood/Controllers/HomeController.cs
@@ -7,20 +7,17 @@ namespace OdeToFood.Controllers
 {
     public class HomeController : Controller
     {
-        public HomeController(IRestaurantData restaurantData, IGreeter gretter)
+        public HomeController(IRestaurantData restaurantData)
         {
             this.restaurantData = restaurantData;
-            this.greeter = gretter;
         }
 
         private IRestaurantData restaurantData;
-        private IGreeter greeter;
 
         public ViewResult Index()
         {
             var model = new HomePageViewModel();
             model.Restaurants = this.restaurantData.GetAll();
-            model.CurrentGreeting = this.greeter.GetGreeting();
             return this.View(model);
         }
 
---
+++ OdeToFood/src/OdeToFood/ViewComponents/Greeting.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNet.Mvc;
+using OdeToFood.Services;
+
+namespace OdeToFood.ViewComponents
+{
+    public class Greeting : ViewComponent
+    {
+        public Greeting(IGreeter greeter)
+        {
+            this.greeter = greeter;
+        }
+
+        private IGreeter greeter;
+
+        public IViewComponentResult Invoke()
+        {
+            var model = this.greeter.GetGreeting();
+            return this.View("Default", model);
+        }
+    }
+}
