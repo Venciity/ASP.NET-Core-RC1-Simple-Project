d6bcd1598d3278ca6c4eccd57a911c263541424d
Ventsislav Yordanov <venciityjordanov@gmail.com>
2016-06-02 02:23:24 +0300

    Added the creating of user
    Added a "UserManager<User>" and a "SignInManager<User>" in
    AccountController.
    Use them to create the user and if the user has created successfully
    then log in the user. Otherwise, we show to the anonymous user error
    messages about the creating of the user account.

--- OdeToFood/src/OdeToFood/Controllers/AccountController.cs
+++ OdeToFood/src/OdeToFood/Controllers/AccountController.cs
@@ -1,12 +1,49 @@
-﻿using Microsoft.AspNet.Mvc;
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Mvc;
+using OdeToFood.Entities;
+using OdeToFood.ViewModels;
+using System.Threading.Tasks;
 
 namespace OdeToFood.Controllers
 {
     public class AccountController : Controller
     {
+        private UserManager<User> userManager;
+        private SignInManager<User> signInManager;
+
+        public AccountController(UserManager<User> userManager, SignInManager<User> signInManager)
+        {
+            this.userManager = userManager;
+            this.signInManager = signInManager;
+        }
+
         [HttpGet]
         public ViewResult Register()
         {
+            return this.View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = new User() { UserName = model.Username };
+                var result = await this.userManager.CreateAsync(user, model.Password);
+                if (result.Succeeded)
+                {
+                    await this.signInManager.SignInAsync(user, isPersistent: false);
+                    return this.RedirectToAction("index", "home");
+                }
+                else
+                {
+                    foreach (var error in result.Errors)
+                    {
+                        ModelState.AddModelError(string.Empty, error.Description);
+                    }
+                }
+            }
+
             return this.View();
         }
     }
